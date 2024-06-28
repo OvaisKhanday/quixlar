29325e2efc454475de51db49370b7aaac3a94a09
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-06-28 16:14:24 +0530

    add custom font to Logo

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,11 +1,15 @@
 import { cn } from "@/lib/utils";
 import { FC } from "react";
+import localFont from "next/font/local";
 
+const playwriteITModerna = localFont({
+  src: "../../public/PlaywriteITModerna-Regular.ttf",
+});
 interface AppLogoProps extends React.HTMLAttributes<HTMLHeadingElement> {}
 
 const AppLogo: FC<AppLogoProps> = ({ className, ...props }) => {
   return (
-    <h1 {...props} className={cn(`text-green-500 text-3xl font-bold`, className)}>
+    <h1 {...props} className={cn(`text-green-500 text-3xl font-bold m-2 lg:mb-4`, playwriteITModerna.className, className)}>
       Quixlar
     </h1>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,15 @@ import Link from "next/link";
 import GoogleAuthButton from "@/components/GoogleAuthButton";
 import { Session } from "next-auth";
 import SignInOrDashboardButton from "@/components/SignInOrDashboardButton";
+import AppLogo from "@/components/AppLogo";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { data: session } = useSession();
   return (
-    <main className=''>
+    <main className='px-4 py-4 md:py-10'>
+      <AppLogo className='text-4xl lg:text-7xl' />
       <SignInOrDashboardButton />
     </main>
   );
