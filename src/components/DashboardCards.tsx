f4271a2e9f1e5b46173d95ab84bac53da690b247
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-06-28 09:03:55 +0530

    refactor Dashboard cards

---
+++ src/components/DashboardCards.tsx
@@ -0,0 +1,59 @@
+import { FC } from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Progress } from "./ui/progress";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+interface DashboardCardsProps {
+  quizzesCount: number;
+  totalParticipants: number;
+  successRate: number;
+}
+
+const DashboardCards: FC<DashboardCardsProps> = ({ quizzesCount, totalParticipants, successRate }) => {
+  return (
+    <div className='grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 '>
+      <Card className='text-center'>
+        <CardHeader>
+          <CardTitle>Total quizzes</CardTitle>
+          <CardDescription>The total number of quizzes created</CardDescription>
+        </CardHeader>
+        <CardContent className='text-2xl'>
+          <h1>{quizzesCount}</h1>
+        </CardContent>
+      </Card>
+      <Card className='text-center'>
+        <CardHeader>
+          <CardTitle>Total Participants</CardTitle>
+          <CardDescription>The total number of users, who took quizzes</CardDescription>
+        </CardHeader>
+        <CardContent className='text-2xl'>
+          <h1>{totalParticipants}</h1>
+        </CardContent>
+      </Card>
+      <Card className='text-center'>
+        <CardHeader>
+          <CardTitle>Success rate</CardTitle>
+          <CardDescription>Percentage of questions correctly answered by the participants</CardDescription>
+        </CardHeader>
+        <CardContent className='text-center flex flex-row items-center justify-center gap-2'>
+          <p className='text-primary/70'>{`${successRate % 100}%`}</p>
+          <Progress value={successRate % 100} className='h-4' />
+        </CardContent>
+      </Card>
+      <Card className='text-center bg-accent'>
+        <CardHeader>
+          <CardTitle>Add quiz</CardTitle>
+          <CardDescription>Create a new quiz</CardDescription>
+        </CardHeader>
+        <CardContent className='text-center'>
+          <Link href={"/dashboard/quiz"}>
+            <Button>Create</Button>
+          </Link>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default DashboardCards;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,6 +7,7 @@ import { QuizI } from "./quiz";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
+import DashboardCards from "@/components/DashboardCards";
 
 interface DashboardProps {
   session: Session | null;
@@ -23,48 +24,8 @@ export default function Dashboard({ session, quizzes, quizzesCount, totalPartici
     <div>
       <Layout>
         <div className='my-8 mx-auto px-2 max-w-7xl'>
-          <div className='grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8 '>
-            <Card className='text-center'>
-              <CardHeader>
-                <CardTitle>Total quizzes</CardTitle>
-                <CardDescription>The total number of quizzes created</CardDescription>
-              </CardHeader>
-              <CardContent className='text-2xl'>
-                <h1>{quizzesCount}</h1>
-              </CardContent>
-            </Card>
-            <Card className='text-center'>
-              <CardHeader>
-                <CardTitle>Total Participants</CardTitle>
-                <CardDescription>The total number of users, who took quizzes</CardDescription>
-              </CardHeader>
-              <CardContent className='text-2xl'>
-                <h1>{totalParticipants}</h1>
-              </CardContent>
-            </Card>
-            <Card className='text-center'>
-              <CardHeader>
-                <CardTitle>Success rate</CardTitle>
-                <CardDescription>Percentage of questions correctly answered by the participants</CardDescription>
-              </CardHeader>
-              <CardContent className='text-center flex flex-row items-center justify-center gap-2'>
-                <p className='text-primary/70'>{`${successRate % 100}%`}</p>
-                <Progress value={successRate % 100} className='h-4' />
-              </CardContent>
-            </Card>
-            <Card className='text-center bg-accent'>
-              <CardHeader>
-                <CardTitle>Add quiz</CardTitle>
-                <CardDescription>Create a new quiz</CardDescription>
-              </CardHeader>
-              <CardContent className='text-center'>
-                <Link href={"/dashboard/quiz"}>
-                  <Button>Create</Button>
-                </Link>
-              </CardContent>
-            </Card>
-          </div>
-
+          <DashboardCards quizzesCount={quizzesCount} totalParticipants={totalParticipants} successRate={successRate} />
+          <div className='mt-4 md:mt-10' />
           <Quizzes quizzes={quizzes} />
         </div>
       </Layout>
