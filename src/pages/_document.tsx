461cb4237f0fc607381a500e8ed102b28e7feac2
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-06-29 21:19:35 +0530

    change rel from shortcut icon to icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <body>
         <Main />
