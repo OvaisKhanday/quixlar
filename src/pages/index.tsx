25012910256a70562e9e3fad8d557510294d3fe6
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-06-29 09:02:28 +0530

    add initial image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,8 @@ export default function Home() {
   return (
     <main className='px-10 lg:px-5 py-4 md:py-10 max-w-7xl mx-auto'>
       <AppLogo className='text-4xl lg:text-7xl text-center' />
-      <div className='flex flex-col lg:flex-row gap-6 justify-center mt-10 items-center text-center'>
+      <h4 className='text-center font-semibold text-primary/65'>The ultimate online quiz platform</h4>
+      <div className='flex flex-col lg:flex-row gap-6 lg:gap-12 justify-center mt-10 items-center text-center'>
         <div className='text-primary/60'>
           <p>
             Introducing Quixlar, the ultimate online quiz platform designed to make quiz creation and participation a breeze! Whether you’re an
@@ -37,7 +38,7 @@ export default function Home() {
           <SignInOrDashboardButton />
         </div>
 
-        <Image src={"/quiz-take.png"} className='shadow-xl rounded-lg' alt='picture of quiz' width={500} height={700} />
+        <Image src={"/quiz-take.png"} className='shadow-xl rounded-lg md:rotate-12' alt='picture of quiz' width={450} height={670} />
       </div>
       <div className='my-2'>
         <hr />
