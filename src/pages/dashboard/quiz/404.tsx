4e8927065c5ccb0ba389ba3446d56de1e2cbdd13
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-06-29 15:50:44 +0530

    fix the svg link in 404 pages

--- src/pages/dashboard/quiz/404.tsx
+++ src/pages/dashboard/quiz/404.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import notFoundIcon from "../../../public/404-page-not-found.svg";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
@@ -8,7 +7,7 @@ interface NotFoundProps {}
 export default function NotFound({}: NotFoundProps) {
   return (
     <div className='flex flex-col w-screen justify-center items-center mt-4 text-primary/60 px-5 max-w-3xl text-center mx-auto'>
-      <Image src={notFoundIcon} alt='not found' className='h-32 my-10' />
+      <Image src={"/404-page-not-found.svg"} alt='not found' className='h-32 my-10' />
       <h1 className='text-3xl font-bold text-primary/60 mb-4'>404: Quiz Not Found</h1>
       <p>The quiz you requested is not available or existent. Please rectify the URL and try again.</p>
       <Link href='/' className='mt-4'>
