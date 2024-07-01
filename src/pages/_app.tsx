1221430ff646bb1508284185d533b1b2f43519ea
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-07-01 17:28:02 +0530

    add loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { NextPage } from "next";
 import { SessionProvider } from "next-auth/react";
+import NextNProgress from "nextjs-progressbar";
 import { Toaster } from "@/components/ui/toaster";
-
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: React.ReactElement) => React.ReactNode;
 };
@@ -15,6 +15,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <SessionProvider session={session}>
+      <NextNProgress color='#0F172A' options={{ showSpinner: false }} />
       <Toaster />
       {getLayout(<Component {...pageProps} />)}
     </SessionProvider>
